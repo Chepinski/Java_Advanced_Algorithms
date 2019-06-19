e62bd634c18246acba7d1b838782b079e8d15142
Chepinski <enginass@yahoo.com>
2019-06-18 20:02:59 -0400

    Ford Fulkerson Algorithm

---
+++ MaxFlow/FordFulkerson.java
@@ -0,0 +1,64 @@
+package MaxFlow;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class FordFulkerson {
+	
+	private boolean[] marked; //marked[v] = true, there is a path from source to sink in residual graph
+	private Edge[] edgeTo; //edgeTo[v] = edges in the augmenting path
+	private double valueMaxFlow;
+	
+	public FordFulkerson(FlowNetwork flowNetwork, Vertex s, Vertex t) {
+		
+		while(hasAugmentingPath(flowNetwork, s, t)) {
+			double minValue = Double.POSITIVE_INFINITY;
+			
+			for(Vertex v=t;v!= s; v= edgeTo[v.getId()].getOther(v)) {//get the minimum
+				minValue = Math.min(minValue, edgeTo[v.getId()].getResidualCapacity(v));
+			}
+			
+			for(Vertex v = t; v!=s; v=edgeTo[v.getId()].getOther(v)) {
+				edgeTo[v.getId()].addResidualFlowTo(v, minValue);
+			}
+			
+			valueMaxFlow = valueMaxFlow + minValue;
+		}
+	}
+	
+	public boolean isInCut(int index) {
+		return marked[index];
+	}
+	
+	public double getMaxFlow() {
+		return this.valueMaxFlow;
+	}
+
+	private boolean hasAugmentingPath(FlowNetwork flowNetwork, Vertex s, Vertex t) {
+		edgeTo = new Edge[flowNetwork.getNumOfVertices()];
+		marked = new boolean[flowNetwork.getNumOfVertices()];
+		
+		Queue<Vertex> queue = new LinkedList<>();
+		queue.add(s);
+		marked[s.getId()] = true;
+		
+		while(!queue.isEmpty() && !marked[t.getId()]){
+			Vertex v = queue.remove();
+			
+			for(Edge e : flowNetwork.getAdjacenciesList(v)) {
+				Vertex w = e.getOther(v);
+				
+				if(e.getResidualCapacity(w) > 0) {
+					if(!marked[w.getId()]) {
+						edgeTo[w.getId()] = e;
+						marked[w.getId()]=true;
+						queue.add(w);
+					}
+				}
+			}
+		}
+		return marked[t.getId()];
+	}
+}
